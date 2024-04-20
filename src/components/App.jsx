dfefc2e52fdd41168d50495d84dc1b24e7e54abd
therealmaduanusi <84400462+therealmaduanusi@users.noreply.github.com>
2024-04-20 22:02:10 +0100

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Form from "./Form";
+import Login from "./Login";
+var userIsRegistered = false;
+
+function App() {
+  return (
+    <div className="container">{userIsRegistered ? <Login /> : <Form />}</div>
+  );
+}
+
+export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Input from "./Input";
+function Form() {
+  return (
+    <form className="form">
+      <Input type="text" placeholder="Username" />
+      <Input type="password" placeholder="Password" />
+      <input type="password" placeholder="Confirm Password" />
+      <button type="submit">Register</button>
+    </form>
+  );
+}
+
+export default Form;
